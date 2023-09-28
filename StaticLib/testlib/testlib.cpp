c55293072ef77110fed9dc1968b7fa4c010ba408
VanoCry <76244655+VanoCry@users.noreply.github.com>
2023-09-29 01:47:02 +0700

    тестовый коммит

--- StaticLib/testlib/testlib.cpp
+++ StaticLib/testlib/testlib.cpp
@@ -5,9 +5,13 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "rus");
     AviaLib::customer_operations customerOps; // Создаем объект класса customer_operations
+    AviaLib::ticket_operations ticketOps;
     AviaLib::customer default_cast = customerOps.add_customer("John Doe", "123456789", "2023-09-27", "123 Main St");
     default_cast.tel = "88005553535";
     // Теперь у вас есть default_cast, который содержит данные, возвращенные функцией add_customer.
+    AviaLib::ticket cust = ticketOps.input_ticket();
+
     return 0;
 }
