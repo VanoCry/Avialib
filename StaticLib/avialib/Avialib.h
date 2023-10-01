79be4e6a1626ff265ef1f5464e5874b439d44c1c
VanoCry <76244655+VanoCry@users.noreply.github.com>
2023-10-02 02:33:42 +0700

    V2.1-->OOP
    Переделываю под ООП

--- StaticLib/avialib/Avialib.h
+++ StaticLib/avialib/Avialib.h
@@ -1,15 +1,131 @@
 #pragma once
-#include <string>
 #include <vector>
+using namespace std;
 
-// Структура "Заказ"
-struct order {
-    std::string company_name;
-    std::string distributor;
-    int price;
-    int ticket_id; 
+namespace AviaLib {
+    //-----------------------------------------------------------------------------------------
+    class Order {
+    private:
+        string company_name;
+        string distributor;
+        int price;
+        int ticket_id;
+    public:
+        Order();
+        Order(string company_name, string distributor, int price, int ticket_id) {
+            this->company_name = company_name;
+            this->distributor = distributor;
+            this->price = price;
+            this->ticket_id = ticket_id;
+        }
+        string getCompany() {
+            return company_name;
+        }
+        string getDistributor() {
+            return distributor;
+        }
+        int getPrice() {
+            return price;
+        }
+        int getTicketID() {
+            return ticket_id;
+        }
+    };
+    //-----------------------------------------------------------------------------------------
+    class ticket {
+    private:
+        string depart;
+        string destination;
+        string distributor;
+        int price;
+        int ticket_id;
+        string fio;
+    public:
+        ticket();
+        ticket(string depart, string destination, string distributor, int price, int ticket_id, string fio) {
+            this->depart = depart;
+            this->destination = destination;
+            this->distributor = distributor;
+            this->price = price;
+            this->ticket_id = ticket_id;
+            this->fio = fio;
+        }
+        string getDepart() {
+            return depart;
+        }
+        string getDestination() {
+            return destination;
+        }
+        string getDistributor(){
+            return distributor;
+        }
+        int getPrice() {
+            return price;
+        }
+        int getTicketID() {
+            return ticket_id;
+        }
+        string getFIO() {
+            return fio;
+        }
+    };
+    //-----------------------------------------------------------------------------------------
+     class operation {
+private:
+    string ticket_operation; // "выдача билета" или "возврат билета"
+    string operation_date;
+    int ticket_id;
+    int tel;
+public:
+    operation();
+    operation(string ticket_operation, string operation_date, int ticket_id, int tel) {
+        this->ticket_operation = ticket_operation;
+        this->operation_date = operation_date;
+        this->tel = tel;
+        this->ticket_id = ticket_id;
+    }
+    string getOperation() {
+        return ticket_operation;
+    }
+    string getOperationDate() {
+        return operation_date;
+    }
+    int getTicketID() {
+        return ticket_id;
+    }
+    int getTel() {
+        return tel;
+    }
 };
+    //-----------------------------------------------------------------------------------------
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // Структура "Покупатель"
 struct customer {
     std::string fio;
@@ -19,21 +135,13 @@ struct customer {
 };
 
 // Структура "Билет"
-struct ticket {
-    std::string depart;
-    std::string destination;
-    std::string distributor;
-    std::string price;
-    std::string ticket_id;
-    std::string fio; 
-};
 
 // Структура "Операция"
 struct operation {
     std::string ticket_operation; // "выдача билета" или "возврат билета"
     std::string operation_date;
-    int ticket_id; 
-    int tel; 
+    int ticket_id;
+    int tel;
 };
 
 // Структура "Авиа_база_данных"
@@ -42,10 +150,13 @@ struct avia_database {
     std::vector<customer> customer_list;
     std::vector<operation> operation_list;
     std::vector<ticket> ticket_list;
-};
 
-namespace AviaLib {
     
+};*/
+
+
+
+    /*
     class add_operations {
     public:
         static void addOrder(avia_database& db, const order& newOrder);
@@ -67,4 +178,4 @@ namespace AviaLib {
 // Псевдонимы для классов
 using AviaAdd = AviaLib::add_operations;
 using AviaRemove = AviaLib::remove_operations;
-using AviaFind = AviaLib::find_operations;
+using AviaFind = AviaLib::find_operations;*/
--- StaticLib/avialib/avialib.cpp
+++ StaticLib/avialib/avialib.cpp
@@ -1,73 +1,90 @@
 ﻿#include "pch.h"
 #include "framework.h"
 #include "avialib.h"
-#include <string> // Добавьте этот заголовок
+#include <string> 
 #include <iostream>
-
+/*
+* Уже решаю проблему описанную в имейле
+* конструкторы и деструкторы уже прописаны
+*
+*
+*/
 namespace AviaLib {
-    // Операции добавления
-    void addOrder(avia_database& db, const order& newOrder) {
-        db.order_list.push_back(newOrder);
-    }
-
-    void addCustomer(avia_database& db, const customer& newCustomer) {
-        db.customer_list.push_back(newCustomer);
-    }
-
-    void addTicket(avia_database& db, const ticket& newTicket) {
-        db.ticket_list.push_back(newTicket);
-    }
-
-    void addOperation(avia_database& db, const operation& newOperation) {
-        db.operation_list.push_back(newOperation);
-    }
-    // Операция удаления
-    void removeOrderByID(avia_database& db, int orderID) {
-        auto it = std::remove_if(db.order_list.begin(), db.order_list.end(),
-            [orderID](const order& o) { return o.ticket_id == orderID; });
-
-        if (it != db.order_list.end()) {
-            db.order_list.erase(it, db.order_list.end());
-        }
-        else {
-            std::cout << "Ошибка: Заказ с указанным ID не найден." << std::endl;
-        }
-    }
-
-    void removeCustomerByFIO(avia_database& db, const std::string& fio) {
-        auto it = std::remove_if(db.customer_list.begin(), db.customer_list.end(),
-            [fio](const customer& c) { return c.fio == fio; });
-
-        if (it != db.customer_list.end()) {
-            db.customer_list.erase(it, db.customer_list.end());
-        }
-        else {
-            std::cout << "Ошибка: Покупатель с указанным ФИО не найден." << std::endl;
-        }
-    }
-
-    // Операции поиска
-
-    order findOrderByID(const avia_database& db, int orderID) {
-        for (const auto& order : db.order_list) {
-            if (order.ticket_id == orderID) {
-                return order;
-            }
-        }
-        std::cout << "Заказ с указанным ID не существует." << std::endl;
-        return order{}; // Вернуть "пустой" объект order
-    }
-
-    customer findCustomerByFIO(const avia_database& db, const std::string& fio) {
-        for (const auto& customer : db.customer_list) {
-            if (customer.fio == fio) {
-                return customer;
-            }
-        }
-        std::cout << "Пользователь с указанным ФИО не существует." << std::endl;
-        return customer{}; // Вернуть "пустой" объект customer
-    }
-
 
 }
 
+
+
+
+
+
+
+
+
+
+
+/*
+   // Операции добавления
+   void addOrder(avia_database& db, const order& newOrder) {
+       db.order_list.push_back(newOrder);
+   }
+
+   void addCustomer(avia_database& db, const customer& newCustomer) {
+       db.customer_list.push_back(newCustomer);
+   }
+
+   void addTicket(avia_database& db, const ticket& newTicket) {
+       db.ticket_list.push_back(newTicket);
+   }
+
+   void addOperation(avia_database& db, const operation& newOperation) {
+       db.operation_list.push_back(newOperation);
+   }
+   // Операция удаления
+   void removeOrderByID(avia_database& db, int orderID) {
+       auto it = std::remove_if(db.order_list.begin(), db.order_list.end(),
+           [orderID](const order& o) { return o.ticket_id == orderID; });
+
+       if (it != db.order_list.end()) {
+           db.order_list.erase(it, db.order_list.end());
+       }
+       else {
+           std::cout << "Ошибка: Заказ с указанным ID не найден." << std::endl;
+       }
+   }
+
+   void removeCustomerByFIO(avia_database& db, const std::string& fio) {
+       auto it = std::remove_if(db.customer_list.begin(), db.customer_list.end(),
+           [fio](const customer& c) { return c.fio == fio; });
+
+       if (it != db.customer_list.end()) {
+           db.customer_list.erase(it, db.customer_list.end());
+       }
+       else {
+           std::cout << "Ошибка: Покупатель с указанным ФИО не найден." << std::endl;
+       }
+   }
+
+   // Операции поиска
+
+   order findOrderByID(const avia_database& db, int orderID) {
+       for (const auto& order : db.order_list) {
+           if (order.ticket_id == orderID) {
+               return order;
+           }
+       }
+       std::cout << "Заказ с указанным ID не существует." << std::endl;
+       return order{}; // Вернуть "пустой" объект order
+   }
+
+   customer findCustomerByFIO(const avia_database& db, const std::string& fio) {
+       for (const auto& customer : db.customer_list) {
+           if (customer.fio == fio) {
+               return customer;
+           }
+       }
+       std::cout << "Пользователь с указанным ФИО не существует." << std::endl;
+       return customer{}; // Вернуть "пустой" объект customer
+   }
+
+   */
