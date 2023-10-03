f890752b3494fd5b90b160d3cb66a18d5b00f4a2
VanoCry <76244655+VanoCry@users.noreply.github.com>
2023-10-03 23:41:45 +0700

    Revert "Fatal"
    This reverts commit 16b188d3cfb0170cebe2f0346474d00a8e08a975.

--- StaticLib/avialib/Avialib.h
+++ StaticLib/avialib/Avialib.h
@@ -1,44 +1,49 @@
-
 #pragma once
-#include <string>
-
+#include <vector>
+using namespace std;
+/*
+* Уже решаю проблему описанную в имейле
+* конструкторы и деструкторы уже прописаны
+* Сначала создаётся класс покупателя, потом билета, затем
+* Заказа а за ним класс операции
+*/
 namespace AviaLib {
-
+    //-----------------------------------------------------------------------------------------
     class Customer {
     private:
         string fio;
         int tel;
         string date;
         string adress;
-
     public:
-        Customer() = default;
+        Customer();
         Customer(string fio, int tel, string date, string adress);
-        string GetFIO() const;
-        int GetTel() const;
-        string GetDate() const;
-        string GetAdress() const;
-        static Customer InputCustomer();
+        string GetFIO();
+        int GetTel();
+        string GetDate();
+        string GetAdress();
+        void AddCustomerToDB(struct Avia_DataBase* db, Customer& new_cast);
+        Customer InputCustomer();
     };
-
+    //-----------------------------------------------------------------------------------------
     class Order {
     private:
         string company_name;
         string distributor;
         int price;
         int ticket_id;
-
     public:
-        Order() = default;
-        Order(string company_name, const Ticket& ticketObj);
+        Order();
+        Order(string company_name,static Ticket& ticketObj);
         Order(string company_name, string distributor, int price, int ticket_id);
-        string getCompany() const;
-        string getDistributor() const;
-        int getPrice() const;
-        int getTicketID() const;
-        static Order InputOrder();
+        string getCompany();
+        string getDistributor();
+        int getPrice();
+        int getTicketID();
+        void AddOrderToDB(struct Avia_DataBase* db, Order& new_order);
+        Order InputOrder();
     };
-
+    //-----------------------------------------------------------------------------------------
     class Ticket {
     private:
         string depart;
@@ -47,62 +52,56 @@ namespace AviaLib {
         int price;
         int ticket_id;
         string fio;
-
     public:
-        Ticket() = default;
+        Ticket();
         Ticket(string depart, string destination, string distributor, int price, int ticket_id, string fio);
-        Ticket(string depart, string destination, string distributor, int price, int ticket_id, const Customer& new_cust);
-        string getDepart() const;
-        string getDestination() const;
-        string getDistributor() const;
-        int getPrice() const;
-        int getTicketID() const;
-        string getFIO() const;
-        static Ticket InputTicket();
+        Ticket(string depart, string destination, string distributor, int price, int ticket_id, static Customer& new_cust);
+        string getDepart();
+        string getDestination();
+        string getDistributor();
+        int getPrice();
+        int getTicketID();
+        string getFIO();
+        void AddTicketToDB(struct Avia_DataBase* db, struct Ticket& new_ticket);
+        Ticket InputTicket();
     };
-
-    class Operation {
-    private:
-        string ticket_operation;
-        string operation_date;
-        int ticket_id;
-        int tel;
-
-    public:
-        Operation() = default;
-        Operation(string ticket_operation, string operation_date, int tel, int ticketID);
-        Operation(string ticket_operation, string operation_date, const Ticket& new_ticket, const Customer& new_cust);
-        string getOperation() const;
-        string getOperationDate() const;
-        int getTicketID() const;
-        int getTel() const;
-        static Operation InputOperation();
-    };
-
-    class Avia_DataBase {
-    public:
-        Order* order_list;
-        Customer* customer_list;
-        Operation* operation_list;
-        Ticket* ticket_list;
-        size_t order_count;
-        size_t customer_count;
-        size_t operation_count;
-        size_t ticket_count;
-
-    public:
-        Avia_DataBase();
-        ~Avia_DataBase();
-        void PrintCustomerList() const;
-        void PrintTicketList() const;
-        void PrintOrderList() const;
-        void PrintOperationList() const;
-        static Avia_DataBase* createAviaDataBase();
-        void destroyAviaDataBase();
-        void AddCustomerToDB(const Customer& new_cast);
-        void AddOrderToDB(const Order& new_order);
-        void AddTicketToDB(const Ticket& new_ticket);
-        void AddOperationToDB(const Operation& new_op);
+    //-----------------------------------------------------------------------------------------
+     class Operation {
+private:
+    string ticket_operation; // "выдача билета" или "возврат билета"
+    string operation_date;
+    int ticket_id;
+    int tel;
+public:
+    Operation();
+    Operation(string ticket_operation, string operation_date, int tel, int ticketID);
+    Operation(string ticket_operation, string operation_date, static Ticket& new_ticket, static Customer& new_cust);
+    string getOperation();
+    string getOperationDate();
+    int getTicketID();
+    int getTel();
+    void AddOperationToDB(struct Avia_DataBase* db, struct Operation& new_op);
+    Operation InputOperation();
+};
+    //-----------------------------------------------------------------------------------------
+     class Avia_DataBase {
+     public:
+         struct Order* order_list;
+         struct Customer* customer_list;
+         struct Operation* operation_list;
+         struct Ticket* ticket_list;
+         size_t order_count;
+         size_t customer_count;
+         size_t operation_count;
+         size_t ticket_count;
+     public:
+         struct Avia_DataBase* createAviaDataBase();
+         // Функция для освобождения памяти базы данных
+         void destroyAviaDataBase(struct Avia_DataBase* db);
+         void PrintCustomerList();
+         void PrintTicketList();
+         void PrintOrderList();
+         void PrintOperationList();
     };
 }
 
--- StaticLib/testlib/testlib.cpp
+++ StaticLib/testlib/testlib.cpp
@@ -2,15 +2,9 @@
 #include "string"
 #include "..\avialib\Avialib.h"
 using namespace std;
-void menu();
 
 int main()
 {
     setlocale(LC_ALL, "rus");
-    menu();
-}
-
-void menu() {
-    cout << "Меню функций библиотеки";
-    cout << "1.";
+    
 }
